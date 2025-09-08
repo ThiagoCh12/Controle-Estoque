b13108a81823a59f0884f35476c502acdad849c6
ThiagoCh12 <thiagocampanholi11@gmail.com>
2025-09-08 14:38:40 -0300

    Criação da classe Order.

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -2,53 +2,41 @@ package entities;
 
 public class Product {
 	
-	private String name;
+    String name;
 	private double price;
 	private int quantity;
 	
+	
+	
 	public Product(String name, double price, int quantity) {
-		if (name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException("Product name cannot be empty.");
-        }
-        if (price <= 0) {
-            throw new IllegalArgumentException("Price must be greater than zero.");
-        }
-        if (quantity < 0) {
-            throw new IllegalArgumentException("Quantity cannot be negative.");
-        }
-        
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
 	}
 	
 	public Product(String name, double price) {
-		if (name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException("Product name cannot be empty.");
-        }
-        if (price <= 0) {
-            throw new IllegalArgumentException("Price must be greater than zero.");
-        }
-        
 		this.name = name;
 		this.price = price;
 	}
+	
+	
+	
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public double getPrice() {
 		return price;
 	}
-	
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
+
 	public int getQuantity() {
 		return quantity;
 	}
